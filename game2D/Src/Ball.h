82855d73f4d69e5a69683a08584f861e3dedb54a
Madeline Ambrose <ambrose907@gmail.com>
2020-07-18 20:20:32 -0700

    Added comments to Ball.h

--- game2D/Src/Ball.h
+++ game2D/Src/Ball.h
@@ -5,18 +5,31 @@
 
 class Ball : public Entity {
 public:
+    //Initilizes texture, renderer, and varaibles
     Ball(SDL_Renderer* renderer);
+
+    //Delete's object's resources
     ~Ball();
 
+    //
     void Update(float delta);
+
+    //Renders ball to screen
     void Render(float delta);
 
+    //Sets direction of ball
     void SetDirection(float dirX, float dirY);
 
+    //Increases speed of ball
     void IncreaseSpeed();
+
+    //Resets speed of ball to original speed
     void ResetSpeed();
 
+    //Speed of ball
     float speed;
+
+    //Direction of ball's x and y coordinates
     float dirX, dirY;
 
 private:
