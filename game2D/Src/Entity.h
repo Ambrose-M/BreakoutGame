0d922bbc6b39f37859693d2ded1057ea60c10dd6
Madeline Ambrose <ambrose907@gmail.com>
2020-07-18 20:21:21 -0700

    Added comments to Entity.h

--- game2D/Src/Entity.h
+++ game2D/Src/Entity.h
@@ -4,17 +4,24 @@
 
 class Entity {
 public:
+    //Initilizes entity's variables and renderer
     Entity(SDL_Renderer* renderer);
     virtual ~Entity();
 
+    //Entity's x coordinate, y coordinate, width and height
     float x, y, width, height;
 
+    //Update entity
     virtual void Update(float delta);
+
+    //Render entity to screen
     virtual void Render(float delta);
 
+    //Checks for collision with other entity
     bool Collides(Entity* other);
 
 protected:
+    //Used to render object to screen
     SDL_Renderer* renderer;
 
 };
