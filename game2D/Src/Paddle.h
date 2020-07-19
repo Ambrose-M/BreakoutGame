8f4f36b6e3faa91b066ac4e7c251bfd7285f229e
Madeline Ambrose <ambrose907@gmail.com>
2020-07-18 20:11:08 -0700

    Added comments to Entity.h

--- game2D/Src/Paddle.h
+++ game2D/Src/Paddle.h
@@ -4,13 +4,19 @@
 
 class Paddle : public Entity {
 public:
+    //Initilizes paddle texture and variables 
     Paddle(SDL_Renderer* renderer);
+
+    //Deletes paddle's resources
     ~Paddle();
 
+    //Renders paddle to screen
     void Render(float delta);
 
+    //Sticks ball to paddle
     void StickBall(Ball* ball);
 
 private:
+    //Paddle texture
     SDL_Texture* texture;
 };
