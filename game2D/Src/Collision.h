0be7111e2ad3720acd5704add799e5f800d6df63
Madeline Ambrose <ambrose907@gmail.com>
2020-07-18 21:17:30 -0700

    Added comments to Collision.h

--- game2D/Src/Collision.h
+++ game2D/Src/Collision.h
@@ -7,11 +7,15 @@
 class Collision
 {
 public:
+    //Returns reflection of ball off of paddle
     float GetReflection(Paddle* paddle, float hitX);
 
+    //Checks for paddle collisions
     void CheckPaddleCollisions(Paddle* paddle, Ball* ball);
 
+    //Checks for brick collisions
     void CheckBrickCollisions(Board* board, Ball* ball);
 
+    //How the ball responds to brick collisions
     void BallBrickResponse(Ball* ball, int dirIndex);
 };
